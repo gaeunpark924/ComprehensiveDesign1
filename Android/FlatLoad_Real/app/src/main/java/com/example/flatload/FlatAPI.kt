21726fd3a8f5d85349ea6589bb3646b045781e09
gaeunpark924 <qkrrkdms924@gmail.com>
2021-10-04 11:16:10 +0900

    add: 하단 메뉴 추가

--- Android/FlatLoad_Real/app/src/main/java/com/example/flatload/FlatAPI.kt
+++ Android/FlatLoad_Real/app/src/main/java/com/example/flatload/FlatAPI.kt
@@ -2,6 +2,8 @@ package com.example.flatload
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
 import java.io.Serializable
@@ -18,4 +20,20 @@ interface FlatAPI {
 
     @GET("/android/result")
     fun getResult(): Call<String>
+
+    //앱 -> 서버로 위험요소 사진 전송
+    @Multipart
+    @POST("/android/post/upload")
+    fun postImage(
+        @Part photo: MultipartBody.Part,
+        @PartMap data: HashMap<String, RequestBody>
+    ): Call<String>
+
+    @GET("/v1/search/local.json")
+    fun getSearchRocal(
+        @Header("X-Naver-Client-Id") id :String,
+        @Header("X-Naver-Client-Secret") pw :String,
+        //@QueryMap query :HashMap<String, RequestBody>
+        @QueryMap query :HashMap<String, String>
+    ): Call<String>
 }
